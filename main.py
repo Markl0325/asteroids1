42e406bb9d10dc88f849a30b8c792fda55f2c8ad
Markl0325 <Markl0325@outlook.com>
2025-02-17 21:25:22 -0500

    Asteroids update 7

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import pygame
 
 from constants import *
 
-from player import *
+from player import Player
+
+from asteroid import Asteroid
+
+from asteroidfield import AsteroidField
+
+
 
 
 def main():
@@ -14,10 +20,14 @@ def main():
     #group
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     dt = 0
+    asteroid_field = AsteroidField()
     
     # Player Location
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -33,7 +43,7 @@ def main():
         updatable.update(dt)
        
         #fill screen
-        screen.fill("black")
+        screen.fill((0, 0, 0))
 
         #Draw player
         for obj in drawable:
--- player.py
+++ player.py
@@ -16,6 +16,11 @@ class Player(CircleShape):
         self.rotation = 0
 
 
+    #draw player
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -24,15 +29,6 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-
-    #draw player
-    def draw(self, screen):
-        pygame.draw.polygon(
-            screen, 
-            "white",
-            self.triangle(),
-            2
-        )
 
     #player rotation
     def rotate(self, dt):
